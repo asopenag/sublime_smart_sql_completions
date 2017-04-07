ed30397d3fde17ac8fdaa082ededd8a891aaa0ba
asopenag <alex.sopena.91@gmail.com>
2017-04-07 14:07:16 +0200

    Add files via upload

---
+++ sql_smart_completions/asopenag_columns_file.py
@@ -0,0 +1,17 @@
+#built by @asopenag on 4/7/2017 12:32:31 PM
+mColumnsDictionary = {
+  'USERS':
+    [
+          ("propertyUsers_a\tmyClass", "property_Users_a"),
+          ("propertyUsers_b\tmyClass", "property_Users_b"),
+          ("propertyUsers_c\tmyClass", "property_Users_c")
+    ],
+
+  'ORDERS': 
+    [
+          ("propertyOrders_a\tmyClass", "property_Orders_a"),
+          ("propertyOrders_b\tmyClass", "property_Orders_b"),
+          ("propertyOrders_c\tmyClass", "property_Orders_c")
+      ]
+
+}
